a572eded9c35def20b8aa93b0fb6fadb0b926c5a
Michael Huang <michaelhuang88@Outlook.com>
2019-03-10 19:33:30 -0400

    fixed window size

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ from kivy.uix.widget import Widget
 from kivy.clock import Clock
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
-Config.set('graphics', 'width', '700')
-Config.set('graphics', 'height', '800')
+Config.set('graphics', 'width', '500')
+Config.set('graphics', 'height', '621')
 Config.set('graphics', 'resizable', False)
 
 
@@ -35,15 +35,15 @@ colours = [(0,0,0,1),(0,1,0,1),(1,0,0,1),(1,1,1,1)]
 
 for i in range(height):
     for j in range(width):
-        grid[i][j] = node(j*30+50, i*30+50, 29, 29)
+        grid[i][j] = node(j*30+10, i*30+10, 29, 29)
 
 
 def paintGrid(self):
     with self.canvas:
         Color(*colours[3])
-        Rectangle(pos=(0,0), size=(700, 800))
+        Rectangle(pos=(0,0), size=(500, 621))
         Color(.501,.501,.501,1)
-        Rectangle(pos=(40, 40), size=(321, 621))
+        Rectangle(pos=(0, 0), size=(321, 621))
         for i in grid:
             for j in i:
                 Color(*colours[j.col])
