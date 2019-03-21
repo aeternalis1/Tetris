1865268c0585c3a52b436a7db3c820ad379af9ed
Michael Huang <30846300+aeternalis1@users.noreply.github.com>
2019-03-21 13:46:33 -0400

    added tetromino info

--- main.py
+++ main.py
@@ -23,6 +23,14 @@ class node:
         self.col = 0
 
 
+class block:
+    def __init__(self, x, y, t, orient):
+        self.x = x  # coordinates of upper left corner of rotation area
+        self.y = y
+        self.t = t  # type of block
+        self.orient = orient  # orientation of block
+
+
 width = 10
 height = 20
 
@@ -30,14 +38,23 @@ grid = [[None for x in range(width)] for x in range(height)]
 chk = [[0 for x in range(width)] for x in range(height)]
 
 
-colours = [(1,1,1,1),       # 0 - black (empty)
-           (0,1,1,1),       # 1 - cyan (long boi)
-           (0,0,1,1),       # 2 - blue (J piece)
-           (1,0.647,0,1),   # 3 - orange (L piece)
-           (1,1,0,1),       # 4 - yellow (square)
-           (0,0.5,1,1),     # 5 - green (S piece)
-           (1,0,0,1),       # 6 - red (Z piece)
-           (0.5,0,0.5,1)]   # 7 - purple (T piece)
+colours = [(1, 1, 1, 1),       # 0 - black (empty)
+           (0, 1, 1, 1),       # 1 - cyan (long boi)
+           (0, 0, 1, 1),       # 2 - blue (J piece)
+           (1, 0.647, 0, 1),   # 3 - orange (L piece)
+           (1, 1, 0, 1),       # 4 - yellow (square)
+           (0, 0.5, 1, 1),     # 5 - green (S piece)
+           (1, 0, 0, 1),       # 6 - red (Z piece)
+           (0.5, 0, 0.5, 1)]   # 7 - purple (T piece)
+
+# format: rotates in x by x grid, with [a,b], [c,d] ... blocks coloured
+types = [[4, 4, [0, 2], [1, 2], [2, 2], [2, 3]],  # long boi (spawns vertical right)
+         [3, 3, [0, 3], [0, 2], [1, 2], [2, 2]],  # J piece (spawns pointy down)
+         [3, 3, [0, 1], [0, 2], [1, 2], [2, 2]],  # L piece (spawns pointy down)
+         [2, 2, [0, 0], [0, 1], [1, 0], [1, 1]],  # square piece
+         [3, 3, [0, 1], [1, 1], [1, 2], [2, 2]],  # S piece (spawns vertical)
+         [3, 3, [0, 2], [1, 2], [1, 1], [2, 1]],  # Z piece (spawns vertical)
+         [3, 3, [0, 1], [1, 0], [1, 1], [1, 2]]]  # T piece (spawns upside down)
 
 
 for i in range(height):
@@ -63,6 +80,11 @@ class MainApp(App):
         parent = Widget()
         paintGrid(parent)
         return parent
+    def runGame(self):
+        alive = 1
+        score = 0
+        while alive:
+            pass
 
 
 tetris = MainApp()
